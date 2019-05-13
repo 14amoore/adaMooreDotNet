cfcc80f5468b1c5ba1197b389cbddb6a909c41f9
Adam Moore <adamoore@gmail.com>
2019-05-13 15:04:15 -0400

    de-linted scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,34 @@
 $(function() {
-   $(window).scroll(function () {
-     if ($(this).scrollTop() > 4500) {
-       $('body').addClass('changeColor5')
-     }
-     if ($(this).scrollTop() < 4500) {
-       $('body').removeClass('changeColor5')
-     }
-      if ($(this).scrollTop() > 3500) {
-         $('body').addClass('changeColor4')
-      }
-      if ($(this).scrollTop() < 3500) {
-         $('body').removeClass('changeColor4')
-      }
-      if ($(this).scrollTop() > 2350) {
-         $('body').addClass('changeColor3')
-      }
-      if ($(this).scrollTop() < 2350) {
-         $('body').removeClass('changeColor3')
-      }
-      if ($(this).scrollTop() > 1550) {
-         $('body').addClass('changeColor2')
-      }
-      if ($(this).scrollTop() < 1550) {
-         $('body').removeClass('changeColor2')
-      }
-      if ($(this).scrollTop() > 500) {
-         $('body').addClass('changeColor')
-         }
-      if ($(this).scrollTop() < 500) {
-         $('body').removeClass('changeColor')
-       }
-   });
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 4500) {
+      $('body').addClass('changeColor5');
+    }
+    if ($(this).scrollTop() < 4500) {
+      $('body').removeClass('changeColor5');
+    }
+    if ($(this).scrollTop() > 3500) {
+      $('body').addClass('changeColor4');
+    }
+    if ($(this).scrollTop() < 3500) {
+      $('body').removeClass('changeColor4');
+    }
+    if ($(this).scrollTop() > 2350) {
+      $('body').addClass('changeColor3');
+    }
+    if ($(this).scrollTop() < 2350) {
+      $('body').removeClass('changeColor3');
+    }
+    if ($(this).scrollTop() > 1550) {
+      $('body').addClass('changeColor2');
+    }
+    if ($(this).scrollTop() < 1550) {
+      $('body').removeClass('changeColor2');
+    }
+    if ($(this).scrollTop() > 500) {
+      $('body').addClass('changeColor');
+    }
+    if ($(this).scrollTop() < 500) {
+      $('body').removeClass('changeColor');
+    }
+  });
 });
