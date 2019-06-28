163b76a52683051670237b98e5776e879a8228a0
Adam Moore <adamoore@gmail.com>
2019-06-28 14:17:06 -0400

    Added weeks 3 and 4 to blog, and more colors for scrolling transitions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,29 @@
 $(function() {
   $(window).scroll(function() {
+    if ($(this).scrollTop() > 8800) {
+      $('body').addClass('changeColor9');
+    }
+    if ($(this).scrollTop() < 8800) {
+      $('body').removeClass('changeColor9');
+    }
+    if ($(this).scrollTop() > 7700) {
+      $('body').addClass('changeColor8');
+    }
+    if ($(this).scrollTop() < 7700) {
+      $('body').removeClass('changeColor8');
+    }
+    if ($(this).scrollTop() > 6600) {
+      $('body').addClass('changeColor7');
+    }
+    if ($(this).scrollTop() < 6600) {
+      $('body').removeClass('changeColor7');
+    }
+    if ($(this).scrollTop() > 5600) {
+      $('body').addClass('changeColor6');
+    }
+    if ($(this).scrollTop() < 5600) {
+      $('body').removeClass('changeColor6');
+    }
     if ($(this).scrollTop() > 4500) {
       $('body').addClass('changeColor5');
     }
